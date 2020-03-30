ec7c650e5ef5ec4a0161f95ba3617a407b4d7f68
cortesana <a.chdez2020@gmail.com>
2020-03-30 14:02:40 +0200

    Working

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,1 +1,78 @@
-'use strict';
+'use strict'
+
+const saveBtn = document.querySelector('#saveBtn');
+const retrieveBtn = document.querySelector('#retrieveBtn');
+const ulElem = document.querySelector('#contactList');
+let isFriend;
+let savedContacts = new Array();
+
+function obtainInfofromAPI() {
+  fetch('https://randomuser.me/api/?results=10')
+    .then(response => response.json())
+    .then(data => {
+      let contactListArray = data.results;
+      for(let i = 0; i < contactListArray.length; i++){
+        savedContacts.push({
+
+          id: {
+            value: contactListArray[i].id.value,
+          },
+          picture: {
+            large: contactListArray[i].picture.large
+          },
+          name: {
+            first: contactListArray[i].name.first,
+            last: contactListArray[i].name.last,
+          },
+          location: {
+            city: contactListArray[i].location.city,
+            country: contactListArray[i].location.country,
+          },
+          login: {
+            username: contactListArray[i].login.username,
+          }
+        })
+      }
+      displayInfo(contactListArray);
+
+    })
+}
+
+function addEventListeners() {
+  const contacts = document.querySelectorAll('.contactListItem')
+  for(let contact of contacts){
+    contact.addEventListener('click', selectionOfFriends);
+  }
+}
+
+function selectionOfFriends() {
+  if(isFriend !== 'true' ) {
+    event.currentTarget.classList.add('friend');
+    isFriend = Boolean('true');
+  } else {
+    event.currentTarget.classList.remove('friend');
+  }
+}
+
+function displayInfo(array) {
+  ulElem.innerHTML = '';
+  for (let i = 0; i < 10; i++) {
+    ulElem.innerHTML += `<li class="contactListItem" id="${array[i].id.value}"><div class="img-container"><img src="${array[i].picture.large}"></div><div class="contactInfo"><h2 id="contactName">${array[i].name.first} ${array[i].name.last}</h2><p id="contactLocation">${array[i].location.city}, ${array[i].location.country}</p><p id ="contactUsername">@${array[i].login.username}</p></div></li>`;
+  }
+}
+
+function saveIntoLocalStorage() {
+  localStorage.setItem('savedContacts', JSON.stringify(savedContacts));
+}
+
+function retrieveFromLocalStorage() {
+  const localStorageContacts = localStorage.getItem('savedContacts');
+  if (localStorageContacts != null) {
+    savedContacts = JSON.parse(localStorageContacts);
+    displayInfo(savedContacts);
+  }
+}
+
+obtainInfofromAPI();
+saveBtn.addEventListener('click', saveIntoLocalStorage);
+retrieveBtn.addEventListener('click', retrieveFromLocalStorage);
